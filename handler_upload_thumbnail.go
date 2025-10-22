414f490796482430587583be65dc53ac97985b0d
Rando <randopoolak@gmail.com>
2025-10-22 22:15:40 +0300

    feat: upload vid to s3

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
+	"os"
+	"mime"
+	"encoding/base64"
+	"crypto/rand"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -31,7 +37,61 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+	
+
+	vid, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Video does not exist", err)
+		return
+	}
+	
+	if vid.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", nil)
+		return
+	}
+		
+	contentType := header.Header.Get("Content-Type")
+	if contentType != "image/jpeg" && contentType != "image/png"{
+		respondWithError(w, http.StatusUnauthorized, "Invalid mime type", nil)
+		return
+	}
+
+	ext, err := mime.ExtensionsByType(contentType)
+	if err != nil{
+		respondWithError(w, http.StatusBadRequest, "Error reading file extension", err)
+		return
+	}
+
+	key := make([]byte, 32)
+	rand.Read(key)
+
+	fileName := fmt.Sprintf("%s%s", base64.RawURLEncoding.EncodeToString(key), ext[0])
+	path := filepath.Join(cfg.assetsRoot, fileName)
+	
+	osFile, err := os.Create(path)
+	if err != nil{
+		respondWithError(w, http.StatusBadRequest, "Error creating file", err)
+		return
+	}
+
+	_, err = io.Copy(osFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error populating file", err)
+		return
+	}
+
+	thumbnailUrl := fmt.Sprintf("http://localhost:8091/assets/%s", fileName) 
+	vid.ThumbnailURL = &thumbnailUrl 
+	cfg.db.UpdateVideo(vid)
+
+	respondWithJSON(w, http.StatusOK, vid)
 }
--- handler_upload_video.go
+++ handler_upload_video.go
@@ -2,6 +2,112 @@ package main
 
 import (
 	"net/http"
+	"fmt"
+	"mime"
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"os"
+	"io"
+	
+	"github.com/aws/aws-sdk-go-v2/aws"
+    "github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	fmt.Println("uploading video", videoID, "by user", userID)
+
+	vid, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Video does not exist", err)
+		return
+	}
+
+	if vid.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", nil)
+		return
+	}
+
+	const maxMemory = 1 << 30
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+	
+	contentType := header.Header.Get("Content-Type")
+	if contentType != "video/mp4"{
+		respondWithError(w, http.StatusUnauthorized, "Invalid mime type", nil)
+		return
+	}
+
+	ext, err := mime.ExtensionsByType(contentType)
+	if err != nil{
+		respondWithError(w, http.StatusBadRequest, "Error reading file extension", err)
+		return
+	}
+
+	key := make([]byte, 32)
+	rand.Read(key)
+
+	fileName := fmt.Sprintf("%s%s", base64.RawURLEncoding.EncodeToString(key), ext[0])
+	
+	tmpFile, err := os.CreateTemp("", fileName)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error creating tmp file", err)
+		return
+	}
+	
+	defer os.Remove(tmpFile.Name())
+	defer tmpFile.Close()
+
+	_, err = io.Copy(tmpFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error populating file", err)
+		return
+	}
+	
+	tmpFile.Seek(0, io.SeekStart)
+	input := &s3.PutObjectInput{
+		Bucket: aws.String(cfg.s3Bucket),
+		Key: aws.String(fileName),
+		Body: tmpFile,
+		ContentType: aws.String(contentType),
+	}
+
+	_, err = cfg.s3Client.PutObject(context.TODO(), input)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error failed to upload file", err)
+		return
+	}
+	
+	url := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, fileName)
+	vid.VideoURL = &url
+	cfg.db.UpdateVideo(vid)
+
+	respondWithJSON(w, http.StatusOK, vid)
+}
